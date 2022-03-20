18a4b44eaa82fcd9835d697e8711f12bdba266ca
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-20 23:01:36 +0530

    Added six number

---
+++ src/Vasu/Addition.java
@@ -0,0 +1,15 @@
+package Vasu;
+
+public class Addition {
+    public static void main(String[] args) {
+        int num = 300;
+         int num2 = 80;
+        int num3 = 60;
+        int num4 = 87;
+        int num5 = 65;
+        int num6;
+        num6 = num + num2 + num3 + num4 +num5 ;
+    System.out.println("addition" +num3);
+}
+
+}
